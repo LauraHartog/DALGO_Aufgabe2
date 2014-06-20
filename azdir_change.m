5be5918c66e40d9f4cad4762ec893b98aebcf580
Laura Hartog <laura.hartog@student.jade-hs.de>
2014-06-20 16:59:18 +0200

    Positionieren des Lautsprechers in Echtzeit; kleine Änderungen an Farbe und Positionen in der Gui

--- azdir_change.m
+++ azdir_change.m
@@ -6,6 +6,8 @@ cP = get(data.azi_axes,'Currentpoint');
 x = cP(1,1);
 y = cP(1,2);
 
+set(data.azi_picture_panel, 'Position', [((x+1)/2)-0.04 ((y+1)/2)-0.04 0.08 0.08]);
+
 if x >= 0 && y >= 0
     
     deg = -atan(y/x) + pi/2;
